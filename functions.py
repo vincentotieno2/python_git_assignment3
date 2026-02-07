b6446b72576fb16062732c62950955842e4988dd
vincentotieno2 <vincentotieno2099@gmail.com>
2026-02-07 09:10:24 +0300

    Inital commit

---
+++ functions.py
@@ -0,0 +1,3 @@
+def addnum(num1, num2):
+    return num1+num2
+print(addnum(3,4))
---
+++ password.py
@@ -0,0 +1,11 @@
+correct_password= "1234"
+
+for attempt in range (3):
+    password= input("Enter your  bank password: ")
+    if password == correct_password:
+        print("Access granted")
+        break
+    else :
+        print("wrong password")
+else:
+    print("Account locked. Too many attempts")
