889efd66e6fa7e3c7be28bf5f31367d51e0a4ec0
vincentotieno2 <vincentotieno2099@gmail.com>
2026-02-10 12:27:48 +0300

    changes to functions file

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
+#this is a file containing practice on arrays
 import numpy as np
 arr=np.array(["a","b","c"])
 print(arr)
-
 arr=np.array([[1,2,3,4,5], [6,7,8,9,10]])
 print(arr[0,1])
 print(arr[0,2])
