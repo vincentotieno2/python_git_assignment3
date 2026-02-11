0a1745cde3eea619c50d35d224f548d93a23d8b2
vincentotieno2 <vincentotieno2099@gmail.com>
2026-02-11 10:00:32 +0300

    Update numpy array lessons

--- Array data types.py
+++ Array data types.py
@@ -1,3 +1,11 @@
+#knowing the array data type ( arr.dtype)
 import numpy as np
 arr=np.array([1.9,2.4,2.9,1.3,-1.2])
-print(arr.astype(bool))
+print(arr.dtype)
+
+arr1=np.array([1,2,3,4,5])
+print(arr1.dtype)
+
+#converting array data type  ( arr.astype('data type'))
+newarr=arr.astype('i')
+print(newarr)
--- Joining_numpys.py
+++ Joining_numpys.py
@@ -4,3 +4,8 @@ arr1=np.array([1,2,3])
 arr2=np.array([4,5,6])
 arr=np.concatenate((arr1,arr2))
 print(arr)
+
+arr3=np.array([21,22,23,24,25,26,27,28,29])
+arr4=np.array([30,31,32,33,34,35])
+arr5=np.concatenate((arr3,arr4))
+print(arr5) #[21 22 23 24 25 26 27 28 29 30 31 32 33 34 35]
